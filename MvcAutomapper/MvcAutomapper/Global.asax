2d11350a8a64e9e8a6bdc4c867042aa24ead27e7
jonmunoa <jmunoa91@gmail.com>
2017-10-30 11:07:31 +0100

    ASP.NET MVC first commit.

---
+++ MvcAutomapper/MvcAutomapper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAutomapper.MvcApplication" Language="C#" %>
---
+++ MvcAutomapper/MvcAutomapper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAutomapper.Startup))]
+namespace MvcAutomapper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
