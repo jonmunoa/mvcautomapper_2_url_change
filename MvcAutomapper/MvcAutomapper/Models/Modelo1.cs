786df168b76e79980c488428e39bf1c1cfb89ec2
Jon Munoa Gutierrez <jmunoa@foreworth.com>
2022-09-28 16:14:37 +0200

    Nuevo fichero

---
+++ MvcAutomapper/MvcAutomapper/Models/Modelo1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAutomapper.Models
+{
+    public class Modelo1
+    {
+        public int Id { get; set; }
+
+        public string Model { get; set; }
+
+        public string Name { get; set; }
+    }
+}
