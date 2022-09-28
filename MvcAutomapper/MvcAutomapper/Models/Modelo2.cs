2972e40b0a168d77af2b9395819bc8a53a0530ab
Jon Munoa Gutierrez <jmunoa@foreworth.com>
2022-09-28 16:15:49 +0200

    Nuevo fichero y otro editado

--- MvcAutomapper/MvcAutomapper/Models/Modelo2.cs
+++ MvcAutomapper/MvcAutomapper/Models/Modelo2.cs
@@ -5,16 +5,12 @@ using System.Web;
 
 namespace MvcAutomapper.Models
 {
-    public class Modelo1
+    public class Modelo2
     {
         public int Id { get; set; }
 
         public string Model { get; set; }
 
         public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        public string Order { get; set; }
     }
 }
